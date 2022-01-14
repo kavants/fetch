d1300feb55284bba30fd4b49a8cb8368e8673e2c
kavants <klavants@gmail.com>
2022-01-14 00:15:15 -0600

    ingesting yaml, created variables, ec2 instance

--- launch.py
+++ launch.py
@@ -2,9 +2,34 @@
 import yaml
 import boto3
 
-myclient = boto3.client('ec2', region_name='us-east-2')
+
+#cidr range for devices needing ssh
+cidr_range = '192.168.0.0/20'
 
 with open('config.yaml', 'r') as yam:
-    mydata = yaml.load(yam,Loader=yaml.FullLoader)
 
-myclient.run_instance(**mydata)
+    conf = yaml.safe_load(yam)
+
+    ec2 = boto3.resource('ec2')
+    conf = conf['server']
+    #ideally I would use a for loop as opposed to creating separte variables
+    volume1 = conf['volumes'][0]
+    user1 = conf['users'][0]
+    region = conf['region']
+
+
+    # create a new EC2 instance
+    instances = ec2.create_instances(
+        Placement={'AvailabilityZone': region['region']},
+        InstanceType=conf['instance_type'],
+        MinCount=conf['min_count'],
+        MaxCount=conf['max_count'],
+        BlockDeviceMappings=[
+            {
+                'DeviceName': volume1['device'],
+                'Ebs': {
+                    'VolumeSize': volume1['size_gb']
+                }
+            }
+        ]
+    )
